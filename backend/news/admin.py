e670de4fbd6ed424a2b35d9a141ad735bdfaa06e
Nabin <asadurzamannabin@gmail.com>
2026-08-28 02:31:58 +0100

    Move backend out of frontend project

--- backend/news/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Author, Section, ArticlePage, NewsletterSubscription
-
-admin.site.register((Author, Section, ArticlePage, NewsletterSubscription))
